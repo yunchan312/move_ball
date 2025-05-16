b41609cab0c7010ebc8e922fd79c9be26b1914d1
yunchan312 <phenomenal312@hufs.ac.kr>
2025-05-16 11:01:05 +0900

    update code by kychann

--- move_ball-main/move_ball.py
+++ move_ball-main/move_ball.py
@@ -24,7 +24,11 @@ def main(stdscr):
         if key == curses.KEY_UP and y > 0:
             y -= 1
         elif key == curses.KEY_DOWN and y < height - 1:
-            y += 1    
+            y += 1
+        elif key == curses.KEY_LEFT and x > 0:
+            x -= 1
+        elif key == curses.KEY_RIGHT and x < width - 1:
+            x += 1
         elif key == ord('q'):
             break
 
